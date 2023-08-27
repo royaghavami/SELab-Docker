430e72797ea37fa4adb06536940c91dd5ee81c57
Roya Ghavami <R.Ghavami@Jabama.com>
2023-08-27 21:14:13 +0330

    refactor: remove comments and console logs

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -2,9 +2,7 @@ const db = require("../models");
 const Tutorial = db.tutorials;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Tutorial
 exports.create = (req, res) => {
-    // Validate request
     if (!req.body.title) {
         res.status(400).send({
             message: "Content can not be empty!"
@@ -12,14 +10,11 @@ exports.create = (req, res) => {
         return;
     }
 
-    // Create a Tutorial
     const tutorial = {
         title: req.body.title,
         description: req.body.description,
         published: req.body.published ? req.body.published : false
     };
-
-    // Save Tutorial in the database
     Tutorial.create(tutorial)
         .then(data => {
             res.send(data);
@@ -32,7 +27,6 @@ exports.create = (req, res) => {
         });
 };
 
-// Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
     const title = req.query.title;
     let condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
@@ -49,7 +43,6 @@ exports.findAll = (req, res) => {
         });
 };
 
-// Find a single Tutorial with an id
 exports.findOne = (req, res) => {
     const id = req.params.id;
 
@@ -70,7 +63,6 @@ exports.findOne = (req, res) => {
         });
 };
 
-// Update a Tutorial by the id in the request
 exports.update = (req, res) => {
     const id = req.params.id;
 
@@ -95,7 +87,6 @@ exports.update = (req, res) => {
         });
 };
 
-// Delete a Tutorial with the specified id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
 
@@ -120,7 +111,6 @@ exports.delete = (req, res) => {
         });
 };
 
-// Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
     Tutorial.destroy({
         where: {},
@@ -137,7 +127,6 @@ exports.deleteAll = (req, res) => {
         });
 };
 
-// Find all published Tutorials
 exports.findAllPublished = (req, res) => {
     Tutorial.findAll({ where: { published: true } })
         .then(data => {
--- app/index.js
+++ app/index.js
@@ -21,20 +21,16 @@ db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");
 });
 
-// parse requests of content-type - application/json
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to bezkoder application." });
 });
 
 require("./routes/tutorial.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || '4000';
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
